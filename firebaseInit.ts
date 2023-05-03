8033ae6c50605afc035958a349db1144cfc78b3d
jmeno1011 <jwhltn8282@gmail.com>
2023-05-03 23:37:43 +0900

    edit firebase init

--- firebaseInit.ts
+++ firebaseInit.ts
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
-import { getStorage } from "firebase/storage";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -23,5 +17,3 @@ const app = initializeApp(firebaseConfig);
 export const firebaseInstance = app;
 
 export const database = getDatabase(app);
-
-export const storage = getStorage(app);
