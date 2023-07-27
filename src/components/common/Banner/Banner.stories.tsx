f7d02d6b24e13b8458b778add0ce7ed6a1e69228
jmeno1011 <jwhltn8282@gmail.com>
2023-07-28 01:10:08 +0900

    add comment

--- src/components/common/Banner/Banner.stories.tsx
+++ src/components/common/Banner/Banner.stories.tsx
@@ -5,6 +5,7 @@ import Banner from "./Banner";
 const meta: Meta<typeof Banner> = {
   title: "Common/Banner",
   component: Banner,
+  // TODO: image는 다른 방식으로 접근해야될거같음
   render: () => <Banner srcUrl={bannerUrl} />,
 };
 export default meta;
