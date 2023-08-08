c16438d578eb1c50af9f88392fb4b228e3fbc7f4
jmeno1011 <jwhltn8282@gmail.com>
2023-08-09 00:26:50 +0900

    add persist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,15 +8,27 @@ import Header from "@/components/common/Header/Header";
 import { useRouter } from "next/router";
 import App from "next/app";
 import { bannerUrl } from "@/lib/const/common";
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 interface AppInitProps {
   bannerState: boolean;
 }
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   devTools: true,
 });
+
+let persistor = persistStore(store)
 
 export default function MyApp({
   Component,
@@ -26,11 +38,13 @@ export default function MyApp({
   const router = useRouter();
   return (
     <Provider store={store}>
-      <div className="layout">
-        {bannerState && <Banner srcUrl={bannerUrl} />}
-        {router.pathname.includes("/result") ? null : <Header />}
-        <Component {...pageProps} />
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="layout">
+          {bannerState && <Banner srcUrl={bannerUrl} />}
+          {router.pathname.includes("/result") ? null : <Header />}
+          <Component {...pageProps} />
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
